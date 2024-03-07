0d73590708b2822c3959c45b80ffbe414c6391f6
de'all <137359388+Alizaaaja4@users.noreply.github.com>
2024-03-07 14:30:00 +0700

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my repository! I'm glad you visited.");
+        System.out.println("Please feel free to contact me if you'd like to start collaborating with me 😀");
+    }
+}
